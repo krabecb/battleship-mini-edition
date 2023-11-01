c999da998b0eca50618b4190ac4155ac4e2e6631
krabecb <briankrabec@gmail.com>
2023-10-31 23:08:52 -0500

    Player can place ships

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//RENDER GAME AREA
 const gameContainer = document.createElement('div')
 gameContainer.setAttribute('class', 'game-container')
 document.body.append(gameContainer)
@@ -10,8 +11,10 @@ const playerContainer = document.createElement('div')
 playerContainer.setAttribute('class', 'player-container')
 gameContainer.append(playerContainer)
 
+
+
 const populateBotSquares = () => {
-    for(let i = 0; i < 8; i++) {
+    for (let i = 0; i < 8; i++) {
         const div = document.createElement('div')
         div.setAttribute('class', 'square bot')
         div.setAttribute('id', i)
@@ -21,7 +24,7 @@ const populateBotSquares = () => {
 populateBotSquares()
 
 const populatePlayerSquares = () => {
-    for(let i = 0; i < 8; i++) {
+    for (let i = 0; i < 8; i++) {
         const div = document.createElement('div')
         div.setAttribute('class', 'square player')
         div.setAttribute('id', i)
@@ -29,3 +32,54 @@ const populatePlayerSquares = () => {
     }
 }
 populatePlayerSquares()
+
+//PROMPT USER TO SELECT SPOTS(2) FOR SHIPS
+const playerObj = {
+    ships: 0,
+}
+const playerSquares = document.querySelectorAll('.player')
+
+const playerSelect = () => {
+    const notification = document.createElement('h3')
+    notification.setAttribute('class', 'notification')
+    notification.innerText = "Select two spots for your ships!"
+    document.body.append(notification)
+}
+playerSelect()
+
+const createShip = (event) => {
+    const id = event.currentTarget.id
+    if (!checkShips()) {
+        playerSquares.forEach(square => {
+            if (id === square.id) {
+                // console.log(`Match: event id: ${id} - square id; ${square.id}`)
+                if(id === '7') {
+                    square.style.backgroundColor = 'black'
+                    playerObj.ships++
+                    return;
+                }
+                square.style.backgroundColor = 'black'
+                playerSquares[parseInt(id) + 1].style.backgroundColor = 'black'
+                playerObj.ships++
+                console.log(playerObj.ships)
+            }
+        })
+    }
+}
+
+const checkShips = () => {
+    if (playerObj.ships === 2) {
+        console.log("Ships at sea!")
+        return true
+    } else {
+        return false
+    }
+}
+
+//EVENT LISTENERS
+playerSquares.forEach(square => {
+    square.addEventListener('click', (e) => {
+        // console.log(`clicked! id: ${e.currentTarget.id}`)
+        createShip(e)
+    })
+})
